a6dce013a26912a8e93baa7e97e5abc0e9e5fe98
KirillPotekhin <kirill.potekhin@gmail.com>
2017-09-28 02:48:26 +0700

    2 экран в1.0

--- js/script.js
+++ js/script.js
@@ -34,3 +34,25 @@ DG.then(function() {
     DG.marker([54.95, 82.89]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
 
 });
+
+$( document ).ready(function() {
+    $('#rangeSlider').rangeslider({
+        polyfill: false,
+        onSlideEnd: function() {
+            console.log(1)
+        },
+        onSlide: function(position, value) {
+            $('.screen2__animation__motor').removeClass('active');
+            $('.screen2__animation__motor.take_' + value).addClass('active');
+            $('.inputRange__item').removeClass('circle__after');
+            $('.inputRange__item').each(function(index, elem) {
+                if (index < value) {
+                    $(elem).addClass('circle__after');
+                }
+            })
+        },
+    });
+
+
+});
+
