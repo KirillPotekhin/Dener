4424b06218a54d8de0ffac80ae2503c8c4e29c97
KirillPotekhin <kirill.potekhin@gmail.com>
2017-10-02 14:26:51 +0700

    готовое в 1

--- js/script.js
+++ js/script.js
@@ -7,33 +7,42 @@
 //     });
 // });
 
-DG.then(function() {
-    var map;
+var mapInited = false;
+
+$(window).on('scroll', function() {
+    if ($(window).scrollTop() > ($('body').height() - $(window).height() * 1.9) && !mapInited){
+        mapInited = true;
+        DG.then(function() {
+            var map;
 
 
 
-    map = DG.map('map', {
-        center: [55.0425, 82.9343],
-        zoom: 16
-    });
+            map = DG.map('map', {
+                center: [55.0425, 82.9343],
+                zoom: 16
+            });
 
-    map.locate({setView: true, watch: true})
-        .on('locationfound', function(e) {
-            DG.marker([e.latitude, e.longitude]).addTo(map);
-        })
-        .on('locationerror', function(e) {
-            DG.popup()
-                .setLatLng(map.getCenter())
-                .setContent('Доступ к определению местоположения отключён')
-                .openOn(map);
+            map.locate({setView: true, watch: true})
+                .on('locationfound', function(e) {
+                    DG.marker([e.latitude, e.longitude]).addTo(map);
+                })
+                .on('locationerror', function(e) {
+                    DG.popup()
+                        .setLatLng(map.getCenter())
+                        .setContent('Доступ к определению местоположения отключён')
+                        .openOn(map);
+                });
+
+            DG.marker([55.041601, 82.917294]).addTo(map).bindPopup('Дядя Дёнер<br>сеть кафе быстрого питания<br>Адрес:<br>Телефон предзаказа: 399-1-400<br>Режим работы: круглосуточно');
+            DG.marker([54.987239, 82.902463]).addTo(map).bindPopup('Общага');
+            DG.marker([54.983695, 82.892536]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
+            DG.marker([54.987746, 82.906335]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
+
         });
+    }
+})
 
-    DG.marker([55.041601, 82.917294]).addTo(map).bindPopup('Дядя Дёнер<br>сеть кафе быстрого питания<br>Адрес:<br>Телефон предзаказа: 399-1-400<br>Режим работы: круглосуточно');
-    DG.marker([54.987239, 82.902463]).addTo(map).bindPopup('Общага');
-    DG.marker([54.983695, 82.892536]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
-    DG.marker([54.987746, 82.906335]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
 
-});
 
 // $( document ).ready(function() {
 //     $('#rangeSlider').rangeslider({
@@ -68,9 +77,12 @@ $( document ).ready(function() {
                 $('.screen2__animation__motor.take_' + value).addClass('active');
                 // $('.inputRange__item').removeClass('circle__after');
                 // $('.inputRange__item').each(function(index, elem) {
-                //     if (index%2===0 < value%2===0) {
-                //         $(elem).addClass('circle__after');
-                //     }
+                //     if ((index%2==0) && (value%2==0)) {
+                //         console.log(1)
+                //         // if (index < value) {
+                //         //     $(elem).addClass('circle__after');
+                //         }
+                //     // }
                 // })
             }
 
