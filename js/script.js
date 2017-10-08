3d3f972c6d41a3774825ed91571d25551bf4b145
KirillPotekhin <kirill.potekhin@gmail.com>
2017-10-09 06:21:45 +0700

    десктоп в 3.0

--- js/script.js
+++ js/script.js
@@ -2,39 +2,40 @@ var mapInited = false;
 
 
 $(window).on('scroll', function() {
-    if (($(window).scrollTop() > $(document).height() - $(window).height() * 1.5) && !mapInited){
-        mapInited = true;
-        DG.then(function() {
-            var map;
+    // if (($(window).scrollTop() > $(document).height() - $(window).height() * 1.5) && !mapInited){
+    //     mapInited = true;
+    //     DG.then(function() {
+    //         var map;
+    //
+    //         map = DG.map('map', {
+    //             center: [55.0425, 82.9343],
+    //             zoom: 16
+    //         });
+    //
+    //         map.locate({setView: true, watch: true})
+    //             .on('locationfound', function(e) {
+    //                 DG.marker([e.latitude, e.longitude]).addTo(map);
+    //             })
+    //             .on('locationerror', function(e) {
+    //                 DG.popup()
+    //                     .setLatLng(map.getCenter())
+    //                     .setContent('Доступ к определению местоположения отключён')
+    //                     .openOn(map);
+    //             });
+    //
+    //         DG.marker([55.041601, 82.917294]).addTo(map).bindPopup('Дядя Дёнер<br>сеть кафе быстрого питания<br>Адрес:<br>Телефон предзаказа: 399-1-400<br>Режим работы: круглосуточно');
+    //         DG.marker([54.987239, 82.902463]).addTo(map).bindPopup('Общага');
+    //         DG.marker([54.983695, 82.892536]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
+    //         DG.marker([54.987746, 82.906335]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
+    //
+    //     });
+    // }
 
-            map = DG.map('map', {
-                center: [55.0425, 82.9343],
-                zoom: 16
-            });
-
-            map.locate({setView: true, watch: true})
-                .on('locationfound', function(e) {
-                    DG.marker([e.latitude, e.longitude]).addTo(map);
-                })
-                .on('locationerror', function(e) {
-                    DG.popup()
-                        .setLatLng(map.getCenter())
-                        .setContent('Доступ к определению местоположения отключён')
-                        .openOn(map);
-                });
-
-            DG.marker([55.041601, 82.917294]).addTo(map).bindPopup('Дядя Дёнер<br>сеть кафе быстрого питания<br>Адрес:<br>Телефон предзаказа: 399-1-400<br>Режим работы: круглосуточно');
-            DG.marker([54.987239, 82.902463]).addTo(map).bindPopup('Общага');
-            DG.marker([54.983695, 82.892536]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
-            DG.marker([54.987746, 82.906335]).addTo(map).bindPopup('Дядя денер <br> работаем круглосуточно');
-
-        });
-    }
     var parallaxOffset = $(window).scrollTop() * -0.5;
-    $('.parallax__layer--back').css({'background-position': `0px ${parallaxOffset}px`})
+    $('.parallax__layer--back').css({'background-position': `-86px ${parallaxOffset}px`})
+
+
 })
-
-
 
 // $( document ).ready(function() {
 //     $('#rangeSlider').rangeslider({
@@ -56,6 +57,7 @@ $(window).on('scroll', function() {
 //
 //
 // });
+
 
 $( document ).ready(function() {
         var slide = 0;
@@ -81,9 +83,9 @@ $( document ).ready(function() {
         },
 
         from: 0,
-        to: 14,
+        to: 12,
         step: 1,
-        scale: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],
+        scale: [0,1,2,3,4,5,6,7,8,9,10,11,12],
         format: '%s',
         width: 485,
         showLabels: false,
@@ -93,7 +95,7 @@ $( document ).ready(function() {
     function runTimer() {
         slideInterval = setInterval(function () {
             slider.jRange('setValue', slide.toString());
-            if (slide === '14') {
+            if (slide === '12') {
                 slide = 0;
             } else {
                 slide++;
@@ -113,12 +115,25 @@ $( document ).ready(function() {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
-        speed: 400,
+        speed: 700,
         spaceBetween: 100,
         pagination: '.swiper-pagination',
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         paginationType: 'progress',
+        effect: 'fade',
+        fade: {
+            crossFade: false
+        }
+
+    });
+
+    $('.swiper-button-prev').click(function () {
+        $('.screen3__block2').css({'opacity': '0'})
+    });
+
+    $('.swiper-button-next').click(function () {
+        $('.screen3__block2').css({'opacity': '0'})
     });
 
     $('.range__elem1').hover(function (e) {
@@ -129,30 +144,27 @@ $( document ).ready(function() {
         $('.range__elem3_' + id).addClass('active');
     });
 
-    function callback() {
-        var width = $(window).width();
-        var pageheight = $('#fullpage').height();
-        var rate =   2;
-        var height = width / rate ;
-        // $('.screen1').height(height);
+    // function callback() {
+    //     var width = $(window).width();
+    //     var pageheight = $(window).height();
+    //     var rate =   1.671428571428571;
+    //     var height = width / rate ;
+    //     $('.swiper-container').height(height);
         // $('.background').height(pageheight);
         // if (width<960) {$('body').css('font-size', '10px')};
         // if (width>1300) {$('body').css('font-size', '25px')}
-    }
+    // }
 
-    $(window).on('resize', function() {
-        var width = $(window).width();
-        var rate =   2.627;
-        var height = width / rate ;
-        // $('.screen1').height(height);
-        // if (width<960) {$('body').css('font-size', '5px')};
-        // if (width<460) {$('body').css('font-size', '2px')};
-        // if (width>960) {$('body').css('font-size', '14px')};
+    // $(window).on('resize', function() {
+    //     var width = $(window).width();
+    //     var pageheight = $(window).height() * 0.99;
+    //     var rate =   1.671428571428571;
+    //     // var height = width / rate;
+    //     var height = width / rate;
+    //     $('.swiper-container').height(height);
+    // })
 
-        // if (width>1200) {$('body').css('font-size', '25')}
-    })
-
-    callback();
+    // callback();
 
 });
 
